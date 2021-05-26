c1052e92de56397e31e0da8777c5d2d7061f9d6f
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-26 19:43:54 +0430

    headers added

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ async function getData(url) {
 		for (const { url, name, season, number, image, summary } of data) {
 			createCard(url, name, season, number, image, summary);
 		}
-		return data;
 	} catch (error) {
 		//TODO do sth to catch errors
 		console.log(error);
@@ -16,8 +15,10 @@ async function getData(url) {
 function createCard(url, name, season, number, image, summary) {
 	const container = document.querySelector("main");
 	const card = document.createElement("div");
+	card.className = "card shadow";
 
 	const header = document.createElement("div");
+	header.className = "card-header text-center border border-3 border-success rounded-pill"
 	header.append(`${name} - S${getEpisodeNum(season)}E${getEpisodeNum(number)}`);
 	card.appendChild(header);
 
